d588ebb3d984427eb7174a50be2f29029c6b5833
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2026-06-26 21:28:20 -0300

    Initial configuration

--- GEstBackendAPI/Program.cs
+++ GEstBackendAPI/Program.cs
@@ -1,24 +1,122 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// 3. Corresponds to: ConfigureServices(IServiceCollection services)
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddSpaStaticFiles(configuration: options =>
+{
+    options.RootPath = "wwwroot";
+});
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("VueCorsPolicy", builder =>
+    {
+        builder
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins("https://localhost:5001");
+    });
+});
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["Okta:Authority"];
+        options.Audience = "api://default";
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+            )
+        };
+    });
+
+builder.Services.AddMvcCore(options => options.EnableEndpointRouting = false);
+builder.Services.AddControllers();
+//builder.Services.AddScoped<IMyService, Service>();
+// End 2.
+
 var app = builder.Build();
+
+// 3. Corresponds to: Configure(IApplicationBuilder app, IWebHostEnvironment env)
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.MapOpenApi();
 }
 
+// Configuring Middlewares
+app.UseCors("VueCorsPolicy");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the Database");
+    }
+}
+
+app.UseAuthentication();
+app.UseMvc();
+app.UseRouting();
+app.UseAuthorization();
+app.UseSpaStaticFiles();
+app.UseSpa(configuration: builder =>
+{
+    if (app.Environment.IsDevelopment())
+    {
+        builder.UseProxyToSpaDevelopmentServer("http://localhost:8080");
+    }
+});
+/*
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+*/
+
+// End 3.
 
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
-
+app.MapControllers();
 app.MapGet("/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
